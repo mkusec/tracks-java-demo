b8de700bc7958316a4e1c586b7b967e8de33f58d
mkusec <mkusec@gmail.com>
2018-03-07 18:39:31 +0000

    JSON logging format (for fluentd etc.)

--- src/main/java/com/example/myproject/DemoApplication.java
+++ src/main/java/com/example/myproject/DemoApplication.java
@@ -1,25 +1,12 @@
 package com.example.myproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
-
-@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Autowired
-	TrackRepository tracks;
-
-	public @PostConstruct
-	void init() {
-		tracks.save(new Track("Track the First", 1.234, -5.678));
 	}
 }
